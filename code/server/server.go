5adb715f0e002f21f0d8b0769a26d060d1a3f6c1
Never Zero <167694532+MasCode256@users.noreply.github.com>
2024-10-28 20:33:56 +0300

    Add files via upload

--- code/server/server.go
+++ code/server/server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"sync/atomic"
 	"time"
@@ -96,8 +99,12 @@ func process_msg(msg string) string {
 		} else {
 			ret = "false"
 		}
-	} else if method == "process" {
+	} else if method == "jr" {
 		ret = "sucess:" + tcp(in("data\\methods\\" + tpe), after(msg, '/'))
+	} else if method == "cmd" {
+		ret = "sucess:" + executeCommand("methods\\" + tpe + "\\main.cmd " + afterbefore(msg, '/', '\x00'))
+	} else if method == "exe" {
+		ret = "sucess:" + executeCommand("methods\\" + tpe + "\\main " + afterbefore(msg, '/', '\x00'))
 	}
 
 	return ret
@@ -222,4 +229,38 @@ func tcp(address string, message string) (string) {
 
 	// Возвращаем ответ в виде строки
 	return response
+}
+
+func executeCommand(fullCommand string) string {
+	if fullCommand == "" {
+		return ""
+	}
+
+	fmt.Printf("Executing command: %s\n", fullCommand)
+
+	var cmd *exec.Cmd
+
+	// Определяем команду в зависимости от операционной системы
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd.exe", "/C", fullCommand)
+	} else {
+		cmd = exec.Command("bash", "-c", fullCommand)
+	}
+
+	var out bytes.Buffer
+	var errOut bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &errOut
+
+	err := cmd.Run()
+	if err != nil {
+		// Выводим стандартный вывод и ошибки для диагностики
+		fmt.Println("Ошибка выполнения команды:", err)
+		if errOut.Len() > 0 {
+			fmt.Println("Ошибка вывода:", errOut.String())
+		}
+		return "error:30"
+	}
+
+	return out.String()
 }
